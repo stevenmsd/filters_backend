dfb8d28f7ef49603e9ed5d9a09a63c15d1c25456
Steeven Montalvo <estywen@gmail.com>
2023-09-25 11:57:06 -0500

    feat: Funcion de buscador de los atributos.

--- app/Classes/Search/Property/Filters/UserFilter.php
+++ app/Classes/Search/Property/Filters/UserFilter.php
@@ -5,12 +5,21 @@ namespace App\Classes\Search\Property\Filters;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
-class NameFilter
+class UserFilter
 {
-    public static function apply (Builder $query, Request $request){
+    /* public static function apply (Builder $query, Request $request){
         
-        if ($request->name) {
-            $query->where('name', 'like', '%' . $request->name . '%');
+        if ($request->user) {
+            $query->where('user_id', $request->user);
+        }
+        
+        return $query;
+    } */
+
+    public static function apply(Builder $query, Request $request)
+    {
+        if ($request->input('user')) { // Accede al valor del parámetro "user"
+            $query->where('user_id', $request->input('user')); // Usar input() para obtener el valor
         }
         
         return $query;
